b7e6aeb83da349d78bc5e10b82fc85353c9ab7fb
Ankur Shahi <shahiankur80@gmail.com>
2022-01-07 18:36:53 -0500

    create react element using JSX

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import './index.css';
 
 ReactDOM.render(
   // 1st argument: what we want to create 
-  React.createElement("h1", {style:{color:"blue"}}, "Heyyyy Everyone!"),
+  // React.createElement("h1", {style:{color:"blue"}}, "Heyyyy Everyone!"),
 
+  // use JSX to create elements
+  <ul>
+    <li>Monday</li>
+    <li>Tuesday</li>
+    <li>Wednesday</li>
+  </ul>,
   // 2nd argument: where we want to put it
   document.getElementById('root')
 );
