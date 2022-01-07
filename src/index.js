5379ac39c52314cdf4578e730af5feb0c8420456
Ankur Shahi <shahiankur80@gmail.com>
2022-01-07 18:51:16 -0500

    create react components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from "./App";
 
 ReactDOM.render(
   // 1st argument: what we want to create 
+  
+  // Render a react element
   // React.createElement("h1", {style:{color:"blue"}}, "Heyyyy Everyone!"),
 
   // use JSX to create elements
-  <ul>
-    <li>Monday</li>
-    <li>Tuesday</li>
-    <li>Wednesday</li>
-  </ul>,
+  // <ul>
+  //   <li>Monday</li>
+  //   <li>Tuesday</li>
+  //   <li>Wednesday</li>
+  // </ul>,
+
+  // render a react component
+  <App />,
   // 2nd argument: where we want to put it
   document.getElementById('root')
 );
