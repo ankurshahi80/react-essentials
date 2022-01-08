a4a13143b6fb168159d06f17917193536fc9fcb9
Ankur Shahi <shahiankur80@gmail.com>
2022-01-07 22:23:14 -0500

    handling error

--- src/App.js
+++ src/App.js
@@ -5,24 +5,31 @@ import './App.css';
 
 function App({login}) {
   const [data, setData] = useState(null);
+  const [loading, setLodaing] = useState(false);
+  const [error, setError] = useState(null)
 
   useEffect(()=>{
+    if(!login) return;
+    setLodaing(true);
     fetch(`https://api.github.com/users/${login}`)
       .then((response)=>response.json())
-      .then(setData);
-  },[]);
+      .then(setData)
+      .then(setLodaing(false))
+      .catch(setError);
+  },[login]);
 
-  if(data) {
-    return (
-      <div>
-        <h1>{data.name}</h1>
-        <p>{data.location}</p>
-        <img alt={data.login} src={data.avatar_url} />
-      </div>
-    );
-  } 
+  if(loading) return <h1>Loading...</h1>;
+  if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>
+  if(!data) return null;
+
   
-  return <div> No User Available</div>;
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.location}</p>
+      <img alt={data.login} src={data.avatar_url} />
+    </div>
+  );
 }
 
 export default App;
