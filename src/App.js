e97d73ef23717829badb4ea6b8a7371eaed9c724
Ankur Shahi <shahiankur80@gmail.com>
2022-01-07 19:05:43 -0500

    add component properties

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 
 import './App.css';
 
-function Header() {
+function Header(props) {
+  console.log(props);
   return (
     <header>
-      <h1>Eve's Kitchen</h1>
+      <h1>{props.name}'s Kitchen</h1>
     </header>
   );
 }
 
-function Main() {
+function Main(props) {
   return (
     <section>
-      <p>We server the most delicious food around!</p>
+      <p>We server the most {props.adjective} food around!</p>
     </section>
   );
 }
 
-function Footer() {
+function Footer(props) {
   return (
     <footer>
-      <p>It's true.</p>
+      <p>Copyright {props.year}</p>
     </footer>
   );
 }
@@ -28,9 +29,9 @@ function Footer() {
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <Header name="Ankur"/>
+      <Main adjective="amazing"/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
