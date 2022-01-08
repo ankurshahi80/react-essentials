53be3bb7d22c0125e76df184a1430aac4fa102b5
Ankur Shahi <shahiankur80@gmail.com>
2022-01-07 20:44:46 -0500

    useState to update state variable

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 
-function SecretComponent() {
-  return(
-    <h1>Super secret information for authorized users only</h1>
-  );
-}
-
-
-function RegularComponent() {
-  return(
-    <h1>Everyone can see this component.</h1>
-  );
-}
-
-function App(props) {
-  // if(props.authorized){
-  //   return <SecretComponent />
-  // } else {
-  //   return <RegularComponent />
-  // }
+function App() {
+  const [emotion, setEmotion] = useState("happy");
   return (
     <>
-      {props.authorized ? <SecretComponent /> : <RegularComponent />}
+      <h1>Current emotion is {emotion}.</h1>
+      <button onClick={()=> setEmotion("happy")}>
+        Happy
+      </button>
+      <button onClick={()=>setEmotion("frustrated")}>
+        Frustrate
+      </button>
+      <button onClick={()=> setEmotion("enthusiastic")}>
+        Enthuse
+      </button>
     </>
   )
 };
